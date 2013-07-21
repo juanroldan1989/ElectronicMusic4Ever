d0a539253aa11186b20b9ba964c96a04c6ee5c7a
Juan Roldan <juanroldan1989@gmail.com>
2013-07-21 16:08:49 -0300

    [Initial commit]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElectronicMusicRails::Application.config.secret_token = 'f7db58c12a9449cad1dc4422e090b3a28ac690dd497c86e5bae5050ecfc67c70e7b8ab383228d14626b770d3f697d4821d7527cd87b3cbafc347c26c318b6325'
