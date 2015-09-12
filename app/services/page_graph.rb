45395be308a5eafd55848e2cdbfa4de11d1208e7
Juan Roldan <juanroldan1989@gmail.com>
2015-09-12 19:27:06 -0300

    PageGraph service - notes added

--- app/services/page_graph.rb
+++ app/services/page_graph.rb
@@ -9,3 +9,12 @@ class PageGraph
     @page_graph.get_connection("me", "feed")
   end
 end
+
+
+# client.get_connection('someuser', 'posts',
+#                     {limit: @options[:max_items],
+#                       fields: ['message', 'id', 'from', 'type',
+#                                 'picture', 'link', 'created_time', 'updated_time'
+#                         ]})
+
+# Note: You can pass a ‘type’ hash key with a value of ‘small’, ‘normal’, ‘large’, or ‘square’ to obtain different picture sizes, with the default being ‘square’. Also, you may need the user_photos permission.
