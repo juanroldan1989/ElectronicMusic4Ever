8913225c6d2eb60f8f93c0f13b993f9f8d93aca9
Juan Roldan <juanroldan1989@gmail.com>
2016-04-15 22:34:51 +1200

    Facebook Page Token implemented inside config files with Figaro

--- config/initializers/configuration.rb
+++ config/initializers/configuration.rb
@@ -1,5 +1,4 @@
 # Maps environment variables into configuration settings
 
-# Facebook IDs
-Rails.application.config.facebook_app_id     = Figaro.env.facebook_app_id
-Rails.application.config.facebook_app_secret = Figaro.env.facebook_app_secret
+# Facebook
+Rails.application.config.facebook_page_token = Figaro.env.facebook_page_token
