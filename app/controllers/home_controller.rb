f1e5608df53128a5112d0f9bfa50a12233db3745
Juan Roldan <juanroldan1989@gmail.com>
2016-04-16 22:50:16 +1200

    News view moved into Home folder - News controller removed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,24 @@
 class HomeController < ApplicationController
+  helper_method :news
+
   def index
   end
 
   def about
   end
 
+  def news
+  end
+
   def gallery
   end
 
   def contact
+  end
+
+  private
+
+  def news
+    @news ||= RetrieveNews.new.results
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 ElectronicMusicRails::Application.routes.draw do
   get "/about"    => "home#about"
-  get "/news"     => "news#index"
+  get "/news"     => "home#news"
   get "/gallery"  => "home#gallery"
   get "/contact"  => "home#contact"
-  get "/callback" => "news#callback"
   root :to        => "home#index"
 end
